a42867c9c2d974ac3e61b2d9268fdd2025bdcebe
Viquoc Quan <chuon-chuon@hotmail.com>
2012-11-03 03:30:45 -0400

    Tail bar's height is zero on startup. Images for coin result added

--- Probability Simulator/Probability Simulator/Pages/CoinPage.xaml.cs
+++ Probability Simulator/Probability Simulator/Pages/CoinPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -50,17 +51,22 @@ namespace Probability_Simulator
             this.Frame.Navigate(typeof(MainPage));
         }
 
-        private async void coinFlipB_Click(object sender, RoutedEventArgs e)
+        private void coinFlipB_Click(object sender, RoutedEventArgs e)
         {
-            int numCard = 0;
-            Int32.TryParse(numCoinBox.Text, out numCard);
-            if (Int32.TryParse(numCoinBox.Text, out numCard) != false && numCard <= 1000)   //if input is valid 
+            testInput();
+        }
+
+        private async void testInput()    //test if input is correct
+        {
+            int numCoin = 0;
+            Int32.TryParse(numCoinBox.Text, out numCoin);
+            if (Int32.TryParse(numCoinBox.Text, out numCoin) != false && numCoin <= 1000)   //if input is valid 
             {
-                
-                    coinFlip(numCard);
+
+                coinFlip(numCoin);
             }
 
-            else if (Int32.TryParse(numCoinBox.Text, out numCard) != false && (numCard > 1000 || numCard < 0))  //if input is out of bound
+            else if (Int32.TryParse(numCoinBox.Text, out numCoin) != false && (numCoin > 1000 || numCoin < 0))  //if input is out of bound
             {
                 var messageDialog = new MessageDialog("Please enter a number between 0 and 1000.");
                 messageDialog.Title = "Invalid Input";
@@ -84,9 +90,10 @@ namespace Probability_Simulator
 
             int result = 0;
 
+            //roll as many time as input
             for (int i = 0; i < numFlip; i++)
             {
-                result = random.Next(0, 2);
+                result = random.Next(0, 2);     //0 is head, 1 is tail
                 if (result == 1)
                 {
                     numTail++;
@@ -99,6 +106,23 @@ namespace Probability_Simulator
                 }
             }
 
+            //update image
+            BitmapImage coinHeadBrush = new BitmapImage(new Uri("ms-appx:///Assets/penny-head.jpg", UriKind.RelativeOrAbsolute));
+            BitmapImage coinTailBrush = new BitmapImage(new Uri("ms-appx:///Assets/penny-tail.jpg", UriKind.RelativeOrAbsolute));
+            ImageBrush headBrush = new ImageBrush();
+            headBrush.ImageSource = coinHeadBrush;
+            ImageBrush tailBrush = new ImageBrush();
+            tailBrush.ImageSource = coinTailBrush;
+            if (result == 1)    //tail
+            {
+                coinImage.Background = tailBrush;
+            }
+            else     //head
+            {
+                coinImage.Background = headBrush;
+            }
+
+            //update history list
             historyList.Children.Add(new TextBlock() { Text = "  " });
             historyScroll.UpdateLayout();   //make sure historyScroll is update to include the added element
             historyScroll.ScrollToVerticalOffset(historyList.ActualHeight);     //scroll to bottom
@@ -109,12 +133,18 @@ namespace Probability_Simulator
         private void updateGraph()  //update the graph to match probability
         {
             numTotal = numHead + numTail;   //total number of result
-            if (numHead == 0)
+
+            if (numHead == 0 && numTail == 0)   //prevent 0/0
+            {
+                headBar.Height = 0;
+                tailBar.Height = 0;
+            }
+            else if (numHead == 0)  //prevent numTail/0
             {
                 headBar.Height = 0;
                 tailBar.Height = graphBox.ActualHeight; //actualHeight so that proportion stay the same across resolution
             }
-            else if (numTail == 0)
+            else if (numTail == 0)  //prevent numHead/0
             {
                 headBar.Height = graphBox.ActualHeight;
                 tailBar.Height = 0;
@@ -128,11 +158,14 @@ namespace Probability_Simulator
 
         private void graphBox_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            headBar.Height = (graphBox.ActualHeight * (numHead / numTotal));
-            tailBar.Height = (graphBox.ActualHeight * (numTail / numTotal));
+            updateGraph();
+        }
+
+        private void coinImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            testInput();
         }
 
 
-    
     }
 }
